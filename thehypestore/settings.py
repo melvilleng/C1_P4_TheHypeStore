205d311c7a20cf92ca097ecdba65de05b5d58828
Melville <melville1992@gmail.com>
2020-05-22 07:23:01 +0000

    setup base template

--- thehypestore/settings.py
+++ thehypestore/settings.py
@@ -65,7 +65,8 @@ ROOT_URLCONF = 'thehypestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
 
         'APP_DIRS': True,
         'OPTIONS': {
