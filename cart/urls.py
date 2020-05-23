f2da7ab05671d26771da85b8702bf6e88659affa
Melville <melville1992@gmail.com>
2020-05-23 08:50:04 +0000

    add to cart and show cart page

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ import cart.views
 
 urlpatterns = [
     path('add_to_cart/<product_id>', cart.views.add_to_cart, name="add_to_cart_route"),
-    path('show_cart', cart.views.show_cart, name="show_cart_route"),
+    path('show_cart/', cart.views.show_cart, name="show_cart_route"),
+    # path('delete_from_cart/<product_id>', cart.views.delete_from_cart, name='delete_from_cart_route')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,redirect,reverse,get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect,reverse
 from product.models import Product
 from django.contrib import messages
 
@@ -8,35 +8,29 @@ def add_to_cart(request, product_id):
     #open file find the key 'shopping cart' in the session
     #if 'shopping cart' is not found return empty dict
     cart = request.session.get('shopping_cart', {})
+
     
-    product = get_object_or_404(Product, pk=product_id)
     #The product not in the cart
     if product_id not in cart:
-        
-
+        product = get_object_or_404(Product, pk=product_id)
         cart[product_id] = {
             'id': product_id,
             'name': product.name,
-            'cost' : 99,
-            'qty' : 1
+            'cost': 99,
+            'qty': 1,
         }
         messages.success(request, f"Product:{product.name} has been added")
 
     #product already in cart
     else:
         cart[product_id]['qty'] += 1
-        messages.success(request, f"One more Product:{product.name} has been added")
     
     #save the session
     request.session['shopping_cart'] = cart
-
-   
-
     return redirect(reverse('show_product_route'))
 
-
 def show_cart(request):
-    #get the cart
+    #Get the session
     cart = request.session.get('shopping_cart')
     return render(request, 'cart/show_cart.template.html',{
         'cart': cart
